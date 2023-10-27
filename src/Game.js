8c8b840752bc353ed5168ce257f689a83bc4b0a4
ProgrammerSteven <Stevenmb1995@gmail.com>
2023-10-26 20:01:02 -0500

    added eventListeners for onKeyDown

--- src/Game.js
+++ src/Game.js
@@ -116,6 +116,7 @@ function Game() {
     translateX: spring(xPos, presets.wobbly),
     translateY: spring(yPos, presets.wobbly),
   };
+
   //time
   const stageRef = useRef(null);
 
@@ -236,6 +237,89 @@ function Game() {
     setStageTime(0);
     stageToggle ? setStageToggle(false) : setStageToggle(true);
   };
+
+  const handleAClick = () => {
+    setStageTime(0);
+    setXPos(level !== 0 ? xArr[level - 1] : 0);
+    setYPos(level !== 0 ? yArr[level - 1] : 25);
+    setCoord(...coordInitial);
+    resetStageTime();
+  };
+  const handleBClick = () => {
+    setlevel(0);
+    setScore(0);
+    setStageTimes([0, 0, 0, 0, 0, 0]);
+    setStageScores([0, 0, 0, 0, 0, 0]);
+    setTime(0);
+    setXPos(0);
+    setYPos(25);
+    setCoord(...coordInitial);
+    resetAllTime();
+  };
+  const handleUpClick = (e) => {
+    //handleSubmitForm(e);
+    if (yPos > 25) setMove([true, false, false, false]);
+    setTimeout(function () {
+      setMove([false, false, false, false]);
+      if (yPos > 25) setYPos(yPos - displayIncrement);
+    }, timeIncrement);
+  };
+  const handleLeftClick = (e) => {
+    //handleSubmitForm(e);
+    if (xPos > 0) setMove([false, false, false, true]);
+    setTimeout(function () {
+      setMove([false, false, false, false]);
+      if (xPos > 0) setXPos(xPos - displayIncrement);
+    }, timeIncrement);
+  };
+  const handleRightClick = (e) => {
+    //handleSubmitForm(e);
+    if (xPos < displayIncrement * 11) setMove([false, true, false, false]);
+    setTimeout(function () {
+      setMove([false, false, false, false]);
+      if (xPos < displayIncrement * 11) setXPos(xPos + displayIncrement);
+    }, timeIncrement);
+  };
+  const handleDownClick = (e) => {
+    //handleSubmitForm(e);
+    if (yPos < displayIncrement * 11) setMove([false, false, true, false]);
+    setTimeout(function () {
+      setMove([false, false, false, false]);
+      if (yPos < displayIncrement * 11) setYPos(yPos + displayIncrement);
+    }, timeIncrement);
+  };
+  const handleKeyDown = (event) => {
+    switch (event.key) {
+      case "ArrowLeft":
+        handleLeftClick();
+        break;
+      case "ArrowRight":
+        handleRightClick();
+        break;
+      case "ArrowUp":
+        handleUpClick();
+        break;
+      case "ArrowDown":
+        handleDownClick();
+        break;
+      case "a":
+      case "A":
+        handleAClick();
+        break;
+      case "b":
+      case "B":
+        handleBClick();
+        break;
+      default:
+        break;
+    }
+  };
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeyDown);
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [xPos, yPos]);
 
   return (
     <div>
@@ -388,8 +472,8 @@ function Game() {
             className="a-buttonContainer btn"
             onClick={() => {
               setStageTime(0);
-              setXPos(level != 0 ? xArr[level - 1] : 0);
-              setYPos(level != 0 ? yArr[level - 1] : 25);
+              setXPos(level !== 0 ? xArr[level - 1] : 0);
+              setYPos(level !== 0 ? yArr[level - 1] : 25);
               setCoord(...coordInitial);
               resetStageTime();
             }}
