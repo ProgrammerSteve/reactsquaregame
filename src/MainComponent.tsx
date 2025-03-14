e38f3608b9954e679e490ebb0f604edb4b436a4d
Steven <stevenmb1995@gmail.com>
2025-03-13 19:08:11 -0500

    turned MainComponent into typescript

---
+++ src/MainComponent.tsx
@@ -0,0 +1,80 @@
+import React,{useState, createContext} from "react";
+import "./MainComponent.css";
+import Game from './Game';
+import Scoreboard from './Scoreboard';
+
+export interface IPayload {
+	score: number;
+	level: number;
+	time: number;
+	stageTime: number;
+	toggle: boolean;
+	stageToggle: boolean;
+	stageScores: number[];
+	stageTimes: number[];
+	setScore: React.Dispatch<React.SetStateAction<number>>;
+	setlevel: React.Dispatch<React.SetStateAction<number>>;		
+	setTime: React.Dispatch<React.SetStateAction<number>>;
+	setStageTime: React.Dispatch<React.SetStateAction<number>>;
+	setToggle: React.Dispatch<React.SetStateAction<boolean>>;
+	setStageToggle: React.Dispatch<React.SetStateAction<boolean>>;
+	setStageScores: React.Dispatch<React.SetStateAction<number[]>>;
+	setStageTimes: React.Dispatch<React.SetStateAction<number[]>>;
+}
+
+const defaultPayload: IPayload = {
+	score: 0,
+	level: 0,
+	time: 0,
+	stageTime: 0,
+	toggle: true,
+	stageToggle: true,
+	stageScores: [0, 0, 0, 0, 0,0],
+	stageTimes: [0, 0, 0, 0, 0, 0],
+	setScore: () => {},
+	setlevel: () => {},	
+	setTime: () => {},
+	setStageTime: () => {},
+	setToggle: () => {},
+	setStageToggle: () => {},
+	setStageScores: () => {},
+	setStageTimes: () => {},
+};
+
+export const Context = createContext<IPayload>(defaultPayload);
+
+const MainComponent=()=>{
+	const[score, setScore]= useState(0);
+	const[level, setlevel]= useState(0);
+	const[time, setTime]= useState(0);
+	const[stageTime, setStageTime]= useState(0);
+	const[toggle,setToggle]=useState(true);
+	const[stageToggle,setStageToggle]=useState(true);
+	const[stageScores, setStageScores]=useState([0,0,0,0,0,0]);
+	const[stageTimes, setStageTimes]=useState([0,0,0,0,0,0]);
+
+	const payload={
+		score,			setScore,
+		level,			setlevel,
+		time,			setTime,
+		stageTime,		setStageTime,
+		toggle,			setToggle,
+		stageToggle,	setStageToggle,
+		stageScores,	setStageScores,
+		stageTimes,		setStageTimes,
+	}
+
+	return(
+	<div className="mainContainer">
+		<Context.Provider value={payload}>
+			<div className="flexContainer">
+				<Game/>
+				<Scoreboard/>	
+			</div>
+		</Context.Provider>
+	</div>
+
+	);
+}
+
+export default MainComponent;
